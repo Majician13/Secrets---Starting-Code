75206bc9ab5a6b8414f9e16a055c2cf88bc3c986
Majician13 <majician13@gmail.com>
2022-04-27 16:43:58 -0400

    Included Facebook OAuth

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 const app = express();
 
@@ -69,6 +70,21 @@ passport.use(
 	)
 );
 
+passport.use(
+	new FacebookStrategy(
+		{
+			clientID: process.env.FB_CLIENT_ID,
+			clientSecret: process.env.FB_CLIENT_SECRET,
+			callbackURL: 'http://localhost:3000/auth/facebook/secrets',
+		},
+		function (accessToken, refreshToken, profile, cb) {
+			User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+				return cb(err, user);
+			});
+		}
+	)
+);
+
 app.get('/', function (req, res) {
 	res.render('home');
 });
@@ -80,6 +96,17 @@ app.get(
 	passport.authenticate('google', { failureRedirect: '/login' }),
 	function (req, res) {
 		// Successful authentication, redirect secrets.
+		res.redirect('/secrets');
+	}
+);
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get(
+	'/auth/facebook/secrets',
+	passport.authenticate('facebook', { failureRedirect: '/login' }),
+	function (req, res) {
+		// Successful authentication, redirect home.
 		res.redirect('/secrets');
 	}
 );
